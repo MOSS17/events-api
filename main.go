e928b7b1cf851e6c40ebb783b973298c53099bcb
Miguel Sandoval <miguel_0799@hotmail.com>
2024-01-19 16:44:37 -0700

    added routes

--- main.go
+++ main.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"events/db"
-	"events/models"
-	"net/http"
+	"events/routes"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,36 +11,7 @@ func main() {
 	db.InitDB()
 	server := gin.Default()
 
-	server.GET("/events", getEvents)
-	server.POST("/events", createEvent)
+	routes.RegisterRoutes(server)
+
 	server.Run(":8080") // localhost:8080
-}
-
-func getEvents(context *gin.Context) {
-	events, err := models.GetAllEvents()
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch events, try again later"})
-		return
-	}
-	context.JSON(http.StatusOK, events)
-}
-
-func createEvent(context *gin.Context) {
-	var event models.Event
-	err := context.ShouldBindJSON(&event)
-
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request"})
-		return
-	}
-
-	event.ID = 1
-	event.UserID = 1
-
-	err = event.Save()
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not create event, try again later" + err.Error()})
-		return
-	}
-	context.JSON(http.StatusCreated, gin.H{"message": "Event created"})
 }
