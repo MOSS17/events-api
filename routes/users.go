fba6e5c4e24586c2a5f204d96ea47763a8adcb53
Miguel Sandoval <miguel_0799@hotmail.com>
2024-01-19 17:05:28 -0700

    create user

---
+++ routes/users.go
@@ -0,0 +1,25 @@
+package routes
+
+import (
+	"events/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func signup(context *gin.Context) {
+	var user models.User
+
+	err := context.ShouldBindJSON(&user)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data" + err.Error()})
+		return
+	}
+
+	err = user.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not save user"})
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "User created succesfully!"})
+}
