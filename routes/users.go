4914ca95c7ce16aa29aa1a4d1e61822d3ecbf3c6
Miguel Sandoval <miguel_0799@hotmail.com>
2024-01-23 14:17:45 -0700

    Login and token

--- routes/users.go
+++ routes/users.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"events/models"
+	"events/utils"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -19,7 +20,34 @@ func signup(context *gin.Context) {
 	err = user.Save()
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not save user"})
+		return
 	}
 
 	context.JSON(http.StatusCreated, gin.H{"message": "User created succesfully!"})
+}
+
+func login(context *gin.Context) {
+	var user models.User
+
+	err := context.ShouldBindJSON(&user)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data"})
+		return
+	}
+
+	err = user.ValidateCredentials()
+
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": err.Error()})
+		return
+	}
+
+	token, err := utils.GenerateToken(user.Email, user.ID)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not authenticate user"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Login succesfull", "token": token})
 }
